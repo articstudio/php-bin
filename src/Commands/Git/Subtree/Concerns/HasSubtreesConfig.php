6ff55697cc81a0c55d55efb4c8cc6a07441e5abd
Mauro Balestra <mbalestra94@gmail.com>
2019-01-28 12:32:28 +0100

    Adding subtree ...

--- src/Commands/Git/Subtree/Concerns/HasSubtreesConfig.php
+++ src/Commands/Git/Subtree/Concerns/HasSubtreesConfig.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Articstudio\PhpBin\Commands\Git\Subtree\Concerns;
 
 use Articstudio\PhpBin\Application;
@@ -9,13 +10,15 @@ trait HasSubtreesConfig
     public function getSubtrees(): array
     {
         $composer = Application::getInstance()->getComposer();
-        $config = $composer['data']['config'] ?? [];
+        $config   = $composer['data']['config'] ?? [];
+
         return $config['subtree'] ?? [];
     }
 
     public function checkPackageInComposer(string $package_name)
     {
         $subtrees = $this->getSubtrees();
+
         return isset($subtrees[$package_name]) ?? false;
     }
 
@@ -23,6 +26,7 @@ trait HasSubtreesConfig
     {
         $cmd = 'git diff --exit-code';
         list($exit_code, $output, $exit_code_txt, $error) = $this->callShell($cmd, false);
+
         return $exit_code === 0 ? false : true;
     }
 }
