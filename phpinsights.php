a6cf55ac2c8c111bafa7c0281634b0f2cdc461df
Marc Mascort Bou <marc@articstudio.com>
2019-05-31 19:45:27 +0200

    Fix class usage

--- phpinsights.php
+++ phpinsights.php
@@ -12,7 +12,7 @@ use SlevomatCodingStandard\Sniffs\ControlStructures\DisallowShortTernaryOperator
 use SlevomatCodingStandard\Sniffs\Functions\UnusedParameterSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\DeclareStrictTypesSniff;
 use SlevomatCodingStandard\Sniffs\TypeHints\DisallowMixedTypeHintSniff;
-use SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff;
+//use SlevomatCodingStandard\Sniffs\TypeHints\TypeHintDeclarationSniff;
 
 return [
 
